8666fb3d2c7736b1c60708232edecc4d9a642fc5
Andrei Dinu <mandrei.dinu@gmail.com>
2014-10-14 23:21:08 +0300

    code refactoring & introduce some examples

--- my_thing.rb
+++ my_thing.rb
@@ -8,16 +8,15 @@ class Bear
 end
 
 class CaptiveBear < Bear
-  attr_reader :size, :color
   def initialize(size = nil, color = nil)
     super(size, color)
-	self.class.track_bear
+    self.class.track_bear
   end
   def self.track_bear
   	@counter += 1 if @counter != nil
   end
   def self.capture(bear) 
-  	new(bear.size, bear.color) # I'm in my own class
+  	new(bear.size, bear.color)  # I'm in my own class
   end
   def self.reset_tracking
     @counter = 0
@@ -26,3 +25,18 @@ class CaptiveBear < Bear
     @counter
   end
 end
+
+# some examples for better basics understanding
+captiveB = CaptiveBear.new(:big, :blue)
+puts "Captive Bear's size: #{captiveB.size}"    # => :big
+puts "Captive Bear's color: #{captiveB.color}"  # => :blue
+normalB = Bear.new(:small, :red)
+captiveB2 = CaptiveBear.capture(normalB)
+puts " #size: I'm normal: #{normalB.size}, I'm captive: #{captiveB2.size}"
+puts "#color: I'm normal: #{normalB.color}, I'm captive: #{captiveB2.color}"
+p CaptiveBear.tracked_bears  # => nil, counter not initialized
+CaptiveBear.reset_tracking   # => @counter = 0
+captiveB3 = CaptiveBear.new(:big, :brown)
+p CaptiveBear.tracked_bears  # => @counter = 1
+captiveB4 = CaptiveBear.new(:vvvBig, :darkBrown)
+p CaptiveBear.tracked_bears  # => @counter = 2
