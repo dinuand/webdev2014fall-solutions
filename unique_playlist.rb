46b0fe3c6eeea2f1a3c96cefa9d4f10f62880919
Andrei Dinu <mandrei.dinu@gmail.com>
2014-10-22 22:33:55 +0300

    solution for unique_playlist_test

---
+++ unique_playlist.rb
@@ -0,0 +1,34 @@
+# load 'song.rb' => use this if you want to print the examples
+
+class UniquePlaylist
+  attr_reader :songs
+  def initialize()
+    @songs = []
+  end
+  def add_song(song)
+    if song.is_a?(Hash)
+      s = Song.new(song)
+      @songs << s
+    else 
+      @songs << song
+    end
+  end
+  def <<(song)
+    if song.is_a?(Hash)
+      s = Song.new(song)
+      if songs.include?(s) == false
+        @songs << s
+      end
+    elsif songs.include?(song) == false
+        @songs << song
+    end
+  end
+end
+
+playlist = UniquePlaylist.new
+song = Song.new({ 'title' => "Misun", 'artist' => "Coffee", 'length' => "256" })
+other_song = Song.new({ 'title' => "Misun", 'artist' => "Coffee", 'length' => '256' })
+playlist << song
+playlist << other_song
+p playlist.songs.count  # => 1
+p playlist.songs[0]
